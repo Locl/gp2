cff5e1dd2a5201f677a946351e55c280d40e4e50
t10052 <t10052@t10052pc.cs.osakac.ac.jp>
2013-10-04 10:44:48 +0900

    function create

--- gp2/prime.cpp
+++ gp2/prime.cpp
@@ -4,5 +4,21 @@
 using namespace std;
 
 int nth_prime(unsigned int a, unsigned int d, unsigned int n){
-	return 0;
+	unsigned int m=0;
+	if(a == 1 && d == 1 && n == 1)return 2;
+	for(unsigned int i = 0;; i += d){
+		bool flag = true;
+		for(unsigned int j = 2; j < (a+i)/2; j++){
+			if((a+i)%j == 0){
+				flag = false;
+				break;
+			}
+		}
+		if(flag == true){
+			m++;
+			if(m==n)return a+i;
+		}
+	}
+	
+	return -1;
 }
