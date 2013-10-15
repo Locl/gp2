e49f321e68ab7f0ac0fcc2dde2807949d61b0364
Locl <locl-ip@i.softbank.jp>
2013-10-15 20:40:13 +0900

    ex2

--- gp2/prime.cpp
+++ gp2/prime.cpp
@@ -22,3 +22,42 @@ int nth_prime(unsigned int a, unsigned int d, unsigned int n){
 	
 	return -1;
 }
+
+int nth_not_prime(unsigned int a, unsigned int d, unsigned int n){
+	unsigned int m=0;
+
+	if(gcd(a,d)==1)return -2;
+
+	for(unsigned int i = 0;; i += d){
+		bool flag = false;
+		if(a+i < 2)flag=true;
+		for(unsigned int j = 2; j < (a+i)/2; j++){
+			if((a+i)%j == 0){
+				flag = true;
+				break;
+			}
+		}
+		if(flag == true){
+			m++;
+			if(m==n)return a+i;
+		}
+	}
+	
+	return -1;
+}
+
+int gcd(unsigned int m, unsigned int n){
+	int tmp;
+
+	if(m<n){
+		tmp = m;
+		m = n;
+		n = tmp;
+	}
+	while(true){
+		if(n==0)return m;
+		tmp = n;
+		n = m%n;
+		m = tmp;
+	}
+}
